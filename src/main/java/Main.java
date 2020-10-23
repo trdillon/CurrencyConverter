4e2c7c80c2b561b886b7e46f05e5a83a1c0eb4f6
Tim Dillon <tim@itsdits.com>
2020-10-23 13:33:38 +0900

    Added placeholder title and logo, fixed window size

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxml/MainView.fxml"));
         primaryStage.setTitle("Currency Converter");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
