249e220b0c026ff89335888396521ee3efb5046e
Tim Dillon <tim@itsdits.com>
2020-10-23 17:18:56 +0900

    Created MainController skeleton and assigned button IDs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/MainView.fxml"));
         primaryStage.setTitle("Currency Converter");
         primaryStage.setScene(new Scene(root, 800, 600));
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,5 +1,73 @@
 package controller;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
 public class MainController {
-    //TODO - implement controller
+
+    @FXML
+    private FontAwesomeIconView btnExit;
+
+    @FXML
+    private FontAwesomeIconView btnHelp;
+
+    @FXML
+    private FontAwesomeIconView btnSettings;
+
+    @FXML
+    private FontAwesomeIconView btnNotify;
+
+    @FXML
+    private FontAwesomeIconView btnHome;
+
+    @FXML
+    private FontAwesomeIconView btnHistory;
+
+    @FXML
+    private FontAwesomeIconView btnFavorites;
+
+    @FXML
+    private FontAwesomeIconView btnData;
+
+    @FXML
+    private void goHelp() {
+        //TODO - help screen
+    }
+
+    @FXML
+    private void goSettings() {
+        //TODO - settings screen
+    }
+
+    @FXML
+    private void goNotify() {
+        //TODO - notification screen
+    }
+
+    @FXML
+    private void goHome() {
+        //TODO - home screen
+    }
+
+    @FXML
+    private void goHistory() {
+        //TODO - history screen
+    }
+
+    @FXML
+    private void goFavorites() {
+        //TODO - favorites screen
+    }
+
+    @FXML
+    private void goData() {
+        //TODO - data screen
+    }
+
+    @FXML
+    private void exitApp() {
+        Stage stage = (Stage) btnExit.getScene().getWindow();
+        stage.close();
+    }
 }
