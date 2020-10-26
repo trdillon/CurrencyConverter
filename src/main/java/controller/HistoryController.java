cb196a69af24ce254ee7b453a3c7e8d22c01e730
Tim Dillon <tim@itsdits.com>
2020-10-26 12:19:08 +0900

    Made HistoryView, added CSS for tables to Styles, added TODO lists

---
+++ src/main/java/controller/HistoryController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class HistoryController {
+
+    @FXML
+    private TableView tvHistory;
+
+    @FXML
+    private TableColumn tcHistoryFrom;
+
+    @FXML
+    private TableColumn tcHistoryTo;
+
+    @FXML
+    private TableColumn tcHistoryResults;
+
+    //TODO - implement History tableview functions
+
+}
--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class HomeController {
+
+    @FXML
+    private ImageView imgFromFlag;
+
+    @FXML
+    private ImageView imgToFlag;
+
+    @FXML
+    private Label lblConvertResult;
+
+    @FXML
+    private Button btnConvert;
+
+    //TODO - implement handleConvert()
+    //TODO - implement flags for imgViews
+    //TODO - implement currency lists
 }
