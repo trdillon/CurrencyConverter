1b76506f44bdae672a15dfe702e84c0f35cb85e3
Tim Dillon <tim@itsdits.com>
2020-11-05 18:31:12 +0900

    Removed the Windows border decoration and made the app window draggable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,32 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import utils.ViewNavigator;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+    private static Stage pStage;
+
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Currency Converter");
-        stage.setScene(createScene(loadMainPane()));
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        setPrimaryStage(primaryStage);
+        getPrimaryStage().setTitle("Currency Converter");
+        getPrimaryStage().initStyle(StageStyle.UNDECORATED);
+        getPrimaryStage().setScene(createScene(loadMainPane()));
+        getPrimaryStage().show();
+    }
+
+    private static Stage getPrimaryStage() {
+        return pStage;
+    }
+
+    private void setPrimaryStage(Stage pStage) {
+        Main.pStage = pStage;
     }
 
     // Load the main view with toolbar and sidebar
@@ -22,6 +37,18 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader();
 
         Pane mainPane = loader.load(getClass().getResourceAsStream(ViewNavigator.MAIN));
+
+        // Make the window draggable
+        mainPane.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        mainPane.setOnMouseDragged(event -> {
+            getPrimaryStage().setX(event.getScreenX() - xOffset);
+            getPrimaryStage().setY(event.getScreenY() - yOffset);
+        });
+
 
         MainController mainController = loader.getController();
         ViewNavigator.setMainController(mainController);
